24ee8ee625a34a212221f955e7ae595316bd15bc
mukai03-colla <mukai-hiroya-fj@ynu.jp>
2025-09-08 11:47:08 +0900

    add

--- app.py
+++ app.py
@@ -2,3 +2,23 @@ import streamlit as st
 
 st.title("Hello, Streamlit!")
 st.write("これは最小構成の Streamlit アプリです。")
+
+number_1 = st.number_input(
+    "1つ目の数字を入力してください。",
+    min_value=0,
+    max_value=100,
+    value=10,
+    step=1
+)
+
+number_2 = st.number_input(
+    "2つ目の数字を入力してください。",
+    min_value=0,
+    max_value=100,
+    value=10,
+    step=1
+)
+
+number = number_1 + number_2
+
+st.write(f"入力された数値は {number} です。")
